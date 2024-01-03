83797ba270b477fa2a574b0b2e3923066a3dff0c
Hubert Lewandowski <therootender@gmail.com>
2024-01-03 14:24:34 +0100

    Gather paths of images

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::{Path, PathBuf};
+use std::error::Error;
+
+fn get_image_paths(path: &Path) -> Result<Vec<PathBuf>, Box<dyn Error>>
+{
+    let mut full_paths: Vec<PathBuf> = vec![];
+
+    for file in path.read_dir()?.filter_map(|e| e.ok())
+    {
+        let filename = file.file_name().to_string_lossy().into_owned();
+        if let Some(extension) = filename.split('.').last()
+        {
+            if extension == "jpg" || extension == "png"
+            {
+                full_paths.push(file.path().canonicalize().unwrap());
+            }
+        }
+    }
+
+    Ok(full_paths)
+}
+
+fn main()
+{
+
 }
