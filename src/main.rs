c2ea1e353272aa3252f70bf6dce4320706348464
Hubert Lewandowski <therootender@gmail.com>
2024-01-04 15:37:49 +0100

    Optimize

--- src/main.rs
+++ src/main.rs
@@ -71,27 +71,25 @@ fn augment_image(image_path: &Path, save_location: &Path) -> Result<(), Box<dyn 
     let ops: Vec<Box<dyn Fn(&mut DynamicImage, &mut ThreadRng)>> = vec![
         Box::new(|_: &mut DynamicImage, _: &mut ThreadRng| {}),
         Box::new(|img: &mut DynamicImage, _: &mut ThreadRng| img.invert()),
-        Box::new(|img: &mut DynamicImage, rng: &mut ThreadRng| { 
-            img.brighten(rng.gen_range(-255..255));
-        }),
-        Box::new(|img: &mut DynamicImage, rng: &mut ThreadRng| { 
-            img.adjust_contrast(rng.gen_range(-1.0..1.0));
-        }),
+        Box::new(|img: &mut DynamicImage, rng: &mut ThreadRng| { img.brighten(rng.gen_range(-255..255)); }),
+        Box::new(|img: &mut DynamicImage, rng: &mut ThreadRng| { img.adjust_contrast(rng.gen_range(-1.0..1.0)); }),
     ];
+
+    fn apply_shift_and_hue_rotate(img: &DynamicImage, rng: &mut ThreadRng) -> DynamicImage {
+        let shift_pixels_v = rng.gen_range(1..img.height());
+        let shift_pixels_h = rng.gen_range(1..img.width());
+        let hue_angle = rng.gen_range(1..360);
+    
+        let shifted_img = shift_image(img, shift_pixels_v, ShiftAxis::Vertical);
+        let shifted_img = shift_image(&shifted_img, shift_pixels_h, ShiftAxis::Horizontal);
+        DynamicImage::from(colorops::huerotate(&shifted_img, hue_angle))
+    }
 
     for (i, transform) in transformations.iter().enumerate() {
         for (j, op) in ops.iter().enumerate() {
-            let shift_pixels_v = rng.gen_range(1..img.height());
-            let shift_pixels_h = rng.gen_range(1..img.width());
-            let hue_angle = rng.gen_range(1..360);
-
-            let shifted_img = shift_image(&img, shift_pixels_v, ShiftAxis::Vertical);
-            let shifted_img = shift_image(&shifted_img, shift_pixels_h, ShiftAxis::Horizontal);
-            let hue_rotated_img = DynamicImage::from(colorops::huerotate(&shifted_img, hue_angle));
-            
-            let mut transformed_img = transform(&hue_rotated_img);
-            
-            op(&mut transformed_img, &mut rng);  // Applying the operation in place
+            let mut transformed_img = transform(&apply_shift_and_hue_rotate(&img, &mut rng));
+        
+            op(&mut transformed_img, &mut rng);
 
             let save_path = save_location.join(format!("augmented_{}_{}.png", i, j));
             transformed_img.save(&save_path)?;
