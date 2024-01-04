96f7429d3f5f0e4de5ce90932a87b230f2b151e0
Hubert Lewandowski <therootender@gmail.com>
2024-01-04 09:41:07 +0100

    Add vertical shift

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use std::io;
 use std::path::{Path, PathBuf};
 use std::error::Error;
+use image::{GenericImageView, DynamicImage, RgbaImage, Pixel};
+
+fn shift_image_vertically(img: &DynamicImage, shift_pixels: u32) -> DynamicImage {
+    let (width, height) = img.dimensions();
+
+    let mut temp_img: RgbaImage = img.to_rgba8();
+
+    for y in 0..height {
+        for x in 0..width {
+            let new_y = (y + shift_pixels) % height;
+            let pixel = img.get_pixel(x, y).to_rgba();
+
+            temp_img.put_pixel(x, new_y, pixel);
+        }
+    }
+
+    DynamicImage::ImageRgba8(temp_img)
+}
 
 fn get_image_paths(path: &Path) -> Result<Vec<PathBuf>, Box<dyn Error>>
 {
@@ -30,7 +48,18 @@ fn main()
     let paths = get_image_paths(Path::new(buffer.trim()))
         .expect("Failed to gather image paths");
 
-    for path in paths {
-        println!("{}", path.display());
+    for path in paths
+    {
+        let img = image::open(path)
+          .expect("Failed to open image");
+
+        let shifted_img = shift_image_vertically(&img, img.height() / 2);
+        let save_path = format!("test_1.png");
+
+        shifted_img
+          .save(&save_path)
+          .expect("Failed to save image");
+
+        println!("Saved: {}", save_path);
     }
 }
