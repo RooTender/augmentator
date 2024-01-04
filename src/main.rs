88618a9b2cd3deade96469910a0913d60481e0b3
Hubert Lewandowski <therootender@gmail.com>
2024-01-04 12:27:15 +0100

    Support more formats

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use image::{DynamicImage, ImageBuffer, Pixel};
+use rand::prelude::*;
+use std::error::Error;
 use std::io;
 use std::path::{Path, PathBuf};
-use std::error::Error;
-use image::{GenericImageView, DynamicImage, RgbaImage, Pixel};
 
 enum ShiftAxis {
     Horizontal,
@@ -9,44 +10,61 @@ enum ShiftAxis {
 }
 
 fn shift_image(img: &DynamicImage, shift_pixels: u32, axis: ShiftAxis) -> DynamicImage {
-    let (width, height) = img.dimensions();
-    let mut temp_img: RgbaImage = img.to_rgba8();
+    fn shift<T: Pixel + 'static + Clone>(
+        img: &ImageBuffer<T, Vec<T::Subpixel>>,
+        shift_pixels: u32,
+        axis: ShiftAxis,
+    ) -> ImageBuffer<T, Vec<T::Subpixel>> {
+        let (width, height) = img.dimensions();
+        let mut temp_img = ImageBuffer::new(width, height);
 
-    match axis {
-        ShiftAxis::Horizontal => {
-            for y in 0..height {
-                let mut row: Vec<_> = (0..width).map(|x| img.get_pixel(x, y).to_rgba()).collect();
-                row.rotate_right(shift_pixels as usize);
-                for (x, pixel) in row.into_iter().enumerate() {
-                    temp_img.put_pixel(x as u32, y, pixel);
+        match axis {
+            ShiftAxis::Horizontal => {
+                for y in 0..height {
+                    let row: Vec<T> = (0..width).map(|x| img.get_pixel(x, y).clone()).collect();
+                    for (x, pixel) in row.into_iter().cycle().skip((width - shift_pixels) as usize).take(width as usize).enumerate() {
+                        temp_img.put_pixel(x as u32, y, pixel);
+                    }
                 }
-            }
-        },
-        ShiftAxis::Vertical => {
-            for x in 0..width {
-                let mut col: Vec<_> = (0..height).map(|y| img.get_pixel(x, y).to_rgba()).collect();
-                col.rotate_right(shift_pixels as usize);
-                for (y, pixel) in col.into_iter().enumerate() {
-                    temp_img.put_pixel(x, y as u32, pixel);
+            },
+            ShiftAxis::Vertical => {
+                for x in 0..width {
+                    let col: Vec<T> = (0..height).map(|y| img.get_pixel(x, y).clone()).collect();
+                    for (y, pixel) in col.into_iter().cycle().skip((height - shift_pixels) as usize).take(height as usize).enumerate() {
+                        temp_img.put_pixel(x, y as u32, pixel);
+                    }
                 }
             }
         }
+
+        temp_img
     }
 
-    DynamicImage::ImageRgba8(temp_img)
+    match img {
+        DynamicImage::ImageLuma8(buf) => DynamicImage::ImageLuma8(shift(buf, shift_pixels, axis)),
+        DynamicImage::ImageLuma16(buf) => DynamicImage::ImageLuma16(shift(buf, shift_pixels, axis)),
+
+        DynamicImage::ImageLumaA8(buf) => DynamicImage::ImageLumaA8(shift(buf, shift_pixels, axis)),
+        DynamicImage::ImageLumaA16(buf) => DynamicImage::ImageLumaA16(shift(buf, shift_pixels, axis)),
+
+        DynamicImage::ImageRgb8(buf) => DynamicImage::ImageRgb8(shift(buf, shift_pixels, axis)),
+        DynamicImage::ImageRgb16(buf) => DynamicImage::ImageRgb16(shift(buf, shift_pixels, axis)),
+        DynamicImage::ImageRgb32F(buf) => DynamicImage::ImageRgb32F(shift(buf, shift_pixels, axis)),
+        
+        DynamicImage::ImageRgba8(buf) => DynamicImage::ImageRgba8(shift(buf, shift_pixels, axis)),
+        DynamicImage::ImageRgba16(buf) => DynamicImage::ImageRgba16(shift(buf, shift_pixels, axis)),
+        DynamicImage::ImageRgba32F(buf) => DynamicImage::ImageRgba32F(shift(buf, shift_pixels, axis)),
+        _ => unimplemented!(),
+    }
 }
 
-fn get_image_paths(path: &Path) -> Result<Vec<PathBuf>, Box<dyn Error>>
-{
+fn get_image_paths(path: &Path) -> Result<Vec<PathBuf>, Box<dyn Error>> {
     let mut full_paths: Vec<PathBuf> = vec![];
 
-    path.read_dir()?.filter_map(|e| e.ok()).for_each(|file|
-    {
+    path.read_dir()?.filter_map(|e| e.ok()).for_each(|file| {
         let filename = file.file_name().to_string_lossy().into_owned();
-        if let Some(extension) = filename.split('.').last()
-        {
-            if extension == "jpg" || extension == "png"
-            {
+        if let Some(extension) = filename.split('.').last() {
+            if extension == "jpg" || extension == "png" {
                 full_paths.push(file.path().canonicalize().unwrap());
             }
         }
@@ -55,17 +73,14 @@ fn get_image_paths(path: &Path) -> Result<Vec<PathBuf>, Box<dyn Error>>
     Ok(full_paths)
 }
 
-fn main()
-{
+fn main() {
     println!("Please enter the directory containing the images: ");
     let mut buffer = String::new();
     let _ = io::stdin().read_line(&mut buffer);
 
-    let paths = get_image_paths(Path::new(buffer.trim()))
-        .expect("Failed to gather image paths");
+    let paths = get_image_paths(Path::new(buffer.trim())).expect("Failed to gather image paths");
 
-    for path in paths
-    {
+    for (i, path) in paths.iter().enumerate() {
         let img = image::open(path)
           .expect("Failed to open image");
 
