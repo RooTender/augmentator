8875d49a023daf9ad99849c34c185a2610813974
RooTender <therootender@gmail.com>
2024-02-18 11:36:25 +0100

    Move application to Tauri

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Greet from './lib/Greet.svelte'
+</script>
+
+<main>
+  <div class="jumbotron text-center pt-5 pb-4">
+    <h1 class="display-5">Augmentation fast & simple.</h1>
+    <p class="lead">This tool is made for the sake of <b>convenience</b> with data augmentation.
+      <br> Pick the transformations you want and simply apply them to your dataset.
+    </p>
+  </div>
+  <hr>
+  
+</main>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+//import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+import App from "./App.svelte";
+
+const app = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
