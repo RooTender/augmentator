a5b988875de2035181bf4e534f821adff4937014
RooTender <therootender@gmail.com>
2024-02-22 16:48:14 +0100

    Add more options

--- src/lib/TransformationsPanel.svelte
+++ src/lib/TransformationsPanel.svelte
@@ -3,19 +3,31 @@
 
     let selectedOption: OptionType = 'everything';
     let advancedOptions = [
+        { id: 'hor_shift', name: 'Horizontal shift', checked: false },
+        { id: 'ver_shift', name: 'Vertical shift', checked: false },
+        { id: 'crop', name: 'Crop', checked: false },
+        { id: 'resize', name: 'Resize', checked: false },
         { id: 'rotate', name: 'Rotate', checked: false },
         { id: 'mirror', name: 'Mirror', checked: false },
         { id: 'flip', name: 'Flip', checked: false },
-        { id: 'hsv_rotation', name: 'HSV rotation', checked: false },
-        { id: 'hor_shift', name: 'Horizontal shift', checked: false },
-        { id: 'ver_shift', name: 'Vertical shift', checked: false },
+        { id: 'saturation', name: 'Saturation shift', checked: false },
+        { id: 'brightness', name: 'Brightness shift', checked: false },
+        { id: 'contrast', name: 'Contrast shift', checked: false },
+        { id: 'hue_rotation', name: 'Hue rotation', checked: false },
+        { id: 'greyscale', name: 'Greyscale', checked: false },
+        { id: 'invert', name: 'Invert colors', checked: false},
+        { id: 'color_norm', name: 'Color norm', checked: false },
     ];
 
     const optionBehaviors = {
         'everything': () => advancedOptions.map(option => ({ ...option, checked: true })),
         'paired': () => advancedOptions.map(option => ({
             ...option,
-            checked: option.id === 'rotate' || option.id === 'mirror',
+            checked: [
+                'hor_shift', 'ver_shift', 'rotate', 'mirror', 'flip', 
+                'saturation', 'brightness', 'contrast', 'hue_rotation',
+                'greyscale', 'invert'
+            ].includes(option.id),
         })),
         'custom': () => advancedOptions
     };
