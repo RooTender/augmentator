cb9db12533f5137140831b9628c8ce0e2ed46928
RooTender <therootender@gmail.com>
2024-03-24 18:35:36 +0100

    Optimize drive usage

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,13 @@
 
 mod transformation_factory;
 mod transformations;
-mod file_handler;
 
 use image::DynamicImage;
 use rand::{rngs::StdRng, SeedableRng};
-use std::{error::Error, fs, path::Path};
+use std::{error::Error, fs, io, path::{Path, PathBuf}};
 use serde::Deserialize;
 
-use crate::{file_handler::*, transformation_factory::*};
+use crate::transformation_factory::*;
 
 #[derive(Debug, Deserialize)]
 struct Directories {
@@ -36,17 +35,16 @@ fn augment_dataset(directories: Directories, transformations: Vec<String>) -> Re
 
     let input_dir = Path::new(directories.input.trim());
     let output_dir = Path::new(directories.output.trim());
-    let preprocessed_dir = output_dir.join("pre-processed");
-    let augmented_dir = output_dir.join("augmented");
 
     let factory = TransformationFactory::new();
 
-    preprocess_data(input_dir, &preprocessed_dir).map_err(|e| e.to_string())?;
+    let image_paths = collect_image_paths(input_dir).map_err(|e| e.to_string())?;
 
     let rng = &mut StdRng::seed_from_u64(1337);
     augment_data(
-        &preprocessed_dir,
-        &augmented_dir,
+        &image_paths,
+        &input_dir,
+        &output_dir,
         &factory,
         &always_transformations,
         &one_time_transformations,
@@ -57,8 +55,31 @@ fn augment_dataset(directories: Directories, transformations: Vec<String>) -> Re
     Ok("Dataset augmentation process started successfully.".into())
 }
 
+fn collect_image_paths(input_dir: &Path) -> io::Result<Vec<PathBuf>> {
+    let mut image_paths = Vec::new();
+
+    for entry in fs::read_dir(input_dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_file() {
+            match image::open(&path) {
+                Ok(_) => {
+                    image_paths.push(path);
+                },
+                Err(_) => {} // Not an image, skip
+            }
+        } else if path.is_dir() {
+            image_paths.extend(collect_image_paths(&path)?);
+        }
+    }
+
+    Ok(image_paths)
+}
+
 fn augment_data(
-    preprocessed_dir: &Path, 
+    image_paths: &Vec<PathBuf>, 
+    input_dir: &Path,
     output_dir: &Path, 
     factory: &TransformationFactory, 
     always_transformations: &[String], 
@@ -67,12 +88,11 @@ fn augment_data(
 ) -> Result<(), Box<dyn Error>> {
     fs::create_dir_all(output_dir)?;
 
-    let paths = collect_file_paths(preprocessed_dir, preprocessed_dir)?;
-    for relative_path in paths {
-        let input_path = preprocessed_dir.join(&relative_path);
-        let output_base_path = output_dir.join(&relative_path).with_extension("");
+    for path in image_paths {
+        let relative_path = path.strip_prefix(input_dir).expect("Error stripping prefix from path");
+        let output_base_path = output_dir.join(relative_path).with_extension("");
 
-        let img = image::open(&input_path)?;
+        let img = image::open(&path)?;
         let mut img = img.clone();
 
         if let Some(parent_dir) = output_base_path.parent() {
