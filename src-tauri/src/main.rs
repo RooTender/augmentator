737e0217581d2bfb5dffe74968aa72362b4d1be5
RooTender <therootender@gmail.com>
2024-03-24 12:13:53 +0100

    Refactor

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -22,6 +22,18 @@ struct Directories {
 fn augment_dataset(directories: Directories, transformations: Vec<String>) -> Result<String, String> {
     check_missing_directories(&directories)?;
 
+    let (always_transformations, one_time_transformations) = transformations.iter().fold(
+        (Vec::new(), Vec::new()),
+        |(mut always, mut one_time), t| {
+            if t == "hor_shift" || t == "ver_shift" {
+                always.push(t.clone());
+            } else {
+                one_time.push(t.clone());
+            }
+            (always, one_time)
+        },
+    );
+
     let input_dir = Path::new(directories.input.trim());
     let output_dir = Path::new(directories.output.trim());
     let preprocessed_dir = output_dir.join("pre-processed");
@@ -38,8 +50,15 @@ fn augment_dataset(directories: Directories, transformations: Vec<String>) -> Re
     preprocess_data(input_dir, &preprocessed_dir, &filters).map_err(|e| e.to_string())?;
 
     let rng = &mut StdRng::seed_from_u64(69);
-    augment_data(&preprocessed_dir, &augmented_dir, &factory, &transformations, rng)
-        .expect("Failed to augment input data!");
+    augment_data(
+        &preprocessed_dir,
+        &augmented_dir,
+        &factory,
+        &always_transformations,
+        &one_time_transformations,
+        rng,
+    )
+    .expect("Failed to augment input data!");
 
     Ok("Dataset augmentation process started successfully.".into())
 }
@@ -48,42 +67,61 @@ fn augment_data(
     input_dir: &Path, 
     output_dir: &Path, 
     factory: &TransformationFactory, 
-    transformations: &[String], 
+    always_transformations: &[String], 
+    one_time_transformations: &[String], 
     rng: &mut StdRng
 ) -> Result<(), Box<dyn Error>> {
     let paths = collect_file_paths(input_dir, input_dir)?;
-    fs::create_dir_all(&output_dir)?;
+    fs::create_dir_all(output_dir)?;
 
-    for path in paths {
+    for path in paths.iter() {
         let full_path = input_dir.join(&path);
-        let img = image::open(&full_path)?;
-    
-        for transformation_name in transformations {
-            if let Some(transformation) = factory.create(transformation_name) {
-                let transformed_img = transformation.apply(&img, rng)?;
-    
-                if let Some(file_name) = path.file_stem().and_then(|name| name.to_str()) {
-                    let extension = path.extension().and_then(|ext| ext.to_str()).unwrap_or("");
-                    let new_file_name = format!("{}_{}.{}", file_name, transformation_name, extension);
-                    
-                    let new_path = if let Some(parent) = output_dir.join(&path).parent() {
-                        fs::create_dir_all(parent)?;
-                        parent.join(new_file_name)
-                    } else {
-                        output_dir.join(new_file_name)
-                    };
-    
-                    transformed_img.save(new_path)?;
-                } else {
-                    eprintln!("Warning: Could not extract filename for path {:?}", path);
-                }
-            } else {
-                eprintln!("Warning: Transformation '{}' not found.", transformation_name);
-            }
+        let mut img = image::open(&full_path)?;
+
+        for transformation_name in always_transformations {
+            img = apply_transformation(&img, factory, transformation_name, rng)?;
+        }
+
+        for transformation_name in one_time_transformations {
+            let transformed_img = apply_transformation(&img, factory, transformation_name, rng)?;
+            save_transformed_image(&transformed_img, &path, output_dir, transformation_name)?;
         }
     }
-    
-    Ok(())    
+
+    Ok(())
+}
+
+fn apply_transformation(
+    img: &image::DynamicImage,
+    factory: &TransformationFactory,
+    transformation_name: &str,
+    rng: &mut StdRng,
+) -> Result<image::DynamicImage, Box<dyn Error>> {
+    match factory.create(transformation_name) {
+        Some(transformation) => transformation.apply(img, rng).map_err(Into::into),
+        None => {
+            println!("Warning: Transformation '{}' not implemented, skipping.", transformation_name);
+            Ok(img.clone()) // Return the original image unchanged
+        }
+    }
+}
+
+fn save_transformed_image(
+    img: &image::DynamicImage,
+    path: &Path,
+    output_dir: &Path,
+    transformation_name: &str,
+) -> Result<(), Box<dyn Error>> {
+    if let Some(file_stem) = path.file_stem().and_then(|name| name.to_str()) {
+        let extension = path.extension().and_then(|ext| ext.to_str()).unwrap_or("png");
+        let new_file_name = format!("{}_{}.{}", file_stem, transformation_name, extension);
+        let new_path = output_dir.join(new_file_name);
+
+        img.save(new_path)?;
+        Ok(())
+    } else {
+        Err("Could not extract filename".into())
+    }
 }
 
 fn check_missing_directories(directories: &Directories) -> Result<(), String> {
